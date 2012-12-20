7ee8dde9902757698e6caf77c36d537ec407dd23
skyhong2008 <skyhong2008@sina.com>
2012-12-20 13:55:10 +0800

    test  add files

---
+++ src/main/java/tiros/com/cn/huwg/spring_mongodb/App.java
@@ -0,0 +1,13 @@
+package tiros.com.cn.huwg.spring_mongodb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
